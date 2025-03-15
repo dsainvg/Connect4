85cb89ab63ca051947594a8e17c0636a086e4342
Praneet Dixit <praneetdixit1234@gmail.com>
2025-03-15 16:22:28 +0530

    Fix: Edge cases

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let cellInfo = [];
 let currentPlayer = 1;
 let wins = [0,0];
+let status = 1;
 
 for(let i = 0;i<7;i++){
     document.getElementById("board").innerHTML += "<div class='col' id='col"+i+"'></div>";
@@ -24,6 +25,7 @@ function restart(){
     });
     currentPlayer = 1;
     document.getElementById("playerTurn").textContent = `Player ${currentPlayer}'s turn`;
+    gameStat = 1;
 }
 
 function stopGame(){
@@ -32,25 +34,28 @@ function stopGame(){
             document.getElementById("cell"+i+j).disabled = true;
         }
     }
+    document.getElementById("playerTurn").textContent = "Game Over! Player "+currentPlayer+" wins!";
     wins[currentPlayer-1]++;
     document.getElementById("player1Score").textContent = `Player 1: ${wins[0]}`;
     document.getElementById("player2Score").textContent = `Player 2: ${wins[1]}`;
+    gameStat = 0;
 }
 
 function checkResult(i,j){
-    console.log("Checking");
-    console.log(i,j);
-    for(let vx = -1;vx<=1;vx++){
-        for(let vy = -1;vy<=1;vy++){
+    for(let vx = 0;vx<=1;vx++){
+        for(let vy = 0;vy<=1;vy++){
             if(vx == 0 && vy == 0) continue;
-            if(cellInfo[i][j] == cellInfo[i+vx][j+vy] && cellInfo[i][j] == cellInfo[i+2*vx][j+2*vy] && cellInfo[i][j] == cellInfo[i+3*vx][j+3*vy]){
-                document.getElementById("playerTurn").textContent = `Player ${cellInfo[i][j]} wins!`;
-                for(let k = 0;k<4;k++){
-                    document.getElementById("cell"+(i+k*vx)+(j+k*vy)).classList.add("blink");
-                    document.getElementById("cell"+(i+k*vx)+(j+k*vy)).textContent = "X";
+            for(let off = -3;off<=0;off++){
+                if(i+off*vx < 0 || i+off*vx > 6 || j+off*vy < 0 || j+off*vy > 5) continue;
+                if(cellInfo[i][j] == cellInfo[i+off*vx][j+off*vy] && cellInfo[i][j] == cellInfo[i+(off+1)*vx][j+(off+1)*vy] && cellInfo[i][j] == cellInfo[i+(off+2)*vx][j+(off+2)*vy] && cellInfo[i][j] == cellInfo[i+(off+3)*vx][j+(off+3)*vy]){
+                    document.getElementById("playerTurn").textContent = `Player ${cellInfo[i][j]} wins!`;
+                    for(let k = 0;k<4;k++){
+                        document.getElementById("cell"+(i+(off+k)*vx)+(j+(off+k)*vy)).classList.add("blink");
+                        document.getElementById("cell"+(i+(off+k)*vx)+(j+(off+k)*vy)).textContent = "X";
+                    }
+                    stopGame();
+                    return;
                 }
-                stopGame();
-                return;
             }
         }
     }
@@ -70,6 +75,6 @@ function nextMove(e){
         document.getElementById("cell"+colId+rowId).classList.add(`player${currentPlayer}Cell`);
         checkResult(colId,rowId);
         currentPlayer = currentPlayer == 1 ? 2 : 1;
-        document.getElementById("playerTurn").textContent = `Player ${currentPlayer}'s turn`;
+        if(gameStat) {document.getElementById("playerTurn").textContent = `Player ${currentPlayer}'s turn`;}
     } 
 }
