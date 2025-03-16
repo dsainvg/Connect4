fe0133a3e8c5aa22361947a6702ad1997afec090
Tanush <tanush.24@kgpian.iitkgp.ac.in>
2025-03-16 13:10:47 +0530

    CSS commit 1

--- script.js
+++ script.js
@@ -128,3 +128,13 @@ function nextMove(e) {
         }
     }
 }
+
+document.getElementById("button").addEventListener("click", function() {
+    const menu = document.getElementById("menu");
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+    }
+    else {
+        menu.style.display = "none";
+    }
+})
